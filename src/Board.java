3fa7bf4c8f7b29fcb5fb1a9c001f2d60779bebfe
Carol Chen <carolyfisher@gmail.com>
2018-01-10 13:37:09 -0500

    UI things     did lots
        deleted data/
        Modified use of third party libraries. MDLAF has some modified files, and many are no longer being used
        Fixed up the welcome panel
        fixed some other random broken things
        Writing very good commit

--- src/Board.java
+++ src/Board.java
@@ -14,45 +14,6 @@ public class Board{
     Board() {
         // Initialize all the squares!
         // Iterate and add to tiles
-        JSONObject json = new JSONObject(getJSON());
-        JSONArray tiles = json.getJSONArray("tiles");
-
-        for (int i = 0; i < tiles.length(); i++){
-            JSONObject tile = tiles.getJSONObject(i);
-            String tileType = tile.getString("type");
-            if (tileType.equals("go")){
-                spots[i] = new GoTile();
-            } else if (tileType.equals("jail")){
-                spots[i] = new JailTile();
-            } else if (tileType.equals("theft")){
-                spots[i] = new TheftTile();
-            } else if (tileType.equals("tax")){
-                spots[i] = new TaxTile(tile.getInt("cost"));
-            } else if (tileType.equals("chance")){
-                spots[i] = new ChanceTile(false);
-            } else if (tileType.equals("wild_chance")){
-                spots[i] = new ChanceTile(true);
-            } else if (tileType.equals("hyperloop")){
-                String tileName = tile.getString("name");
-                spots[i] = new HyperloopTile(tileName);
-            } else if (tileType.equals("property")){
-                String color = tile.getString("color");
-                String tileName = tile.getString("name");
-                int[] rent = {
-                    tile.getJSONObject("rent").getInt("0_avocadoes"),
-                    tile.getJSONObject("rent").getInt("1_avocadoes"),
-                    tile.getJSONObject("rent").getInt("2_avocadoes"),
-                    tile.getJSONObject("rent").getInt("3_avocadoes"),
-                    tile.getJSONObject("rent").getInt("4_avocadoes"),
-                    tile.getJSONObject("rent").getInt("5_avocadoes"),
-                };
-                int mortgage = tile.getInt("mortgage");
-                int cost = tile.getInt("purchase");
-                int avocadoCost = tile.getInt("avocado_cost");
-                spots[i] = new Property(tileName, color, rent,
-                                        mortgage, cost, avocadoCost);
-            }
-        }
 
     }
 
---
+++ src/TilesInit.java
@@ -0,0 +1,110 @@
+class TilesInit {
+    public Tile[] tiles = {
+      new GoTile(),
+      new Property(
+        "The Macintosh",
+        "brown",
+        new int[] { 2, 10, 30, 90, 160, 250 },
+        30,
+        60,
+        50
+      ),
+      new ChanceTile(),
+      new Property(
+        "No World Wars in fifty years™",
+        "brown",
+        new int[] { 4, 20, 60, 180, 320, 450 },
+        30,
+        60,
+        50
+      ),
+      new TaxTile(),
+      new HyperloopTile(),
+      new Property(
+        "Taylor Swift",
+        "light blue",
+        new int[] { 6, 30, 90, 270, 400, 550 },
+        50,
+        100,
+        50
+      ),
+      new ChanceTile(),
+      new Property(
+        "Miley Cyrus",
+        "light blue",
+        new int[] { 6, 30, 90, 270, 400, 550 },
+        50,
+        100,
+        50
+      ),
+      new Property(
+        "Kanye West",
+        "light blue",
+        new int[] { 8, 40, 100, 300, 450, 600 },
+        60,
+        120,
+        50
+      ),
+      new JailTile(),
+      new Property(
+        "Netflix and Chill™",
+        "magenta",
+        new int[] { 10, 50, 150, 450, 625, 750 },
+        70,
+        140,
+        100
+      ),
+      new Wild_chanceTile(),
+      new Property(
+        "Using internet acronyms irl™",
+        "magenta",
+        new int[] { 10, 50, 150, 450, 625, 750 },
+        70,
+        140,
+        100
+      ),
+      new Property(
+        "Paying a friend to take care of your Snap streaks™",
+        "magenta",
+        new int[] { 12, 60, 180, 500, 700, 900 },
+        80,
+        160,
+        100
+      ),
+      new HyperloopTile(),
+      new Property(
+        "Nyan Cat",
+        "orange",
+        new int[] { 14, 70, 200, 550, 750, 950 },
+        90,
+        180,
+        100
+      ),
+      new ChanceTile(),
+      new Property(
+        "Datboi",
+        "orange",
+        new int[] { 14, 70, 200, 550, 750, 950 },
+        90,
+        180,
+        100
+      ),
+      new Property(
+        "It's Wednesday my dudes",
+        "orange",
+        new int[] { 16, 80, 220, 600, 800, 1000 },
+        100,
+        200,
+        100
+      ),
+      new TheftTile(),
+      new Property(
+        "INSERT NAME HERE",
+        "red",
+        new int[] { 2, 10, 30, 90, 160, 250 },
+        30,
+        60,
+        50
+      )
+    };
+  }
