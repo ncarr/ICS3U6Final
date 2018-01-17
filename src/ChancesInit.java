55e361aed4df44ee00663cf71d6a10a4a856e0f9
Nicholas Carr <nicholas.carr@rogers.com>
2018-01-17 17:57:21 -0500

    Added Nathan's chance cards

--- src/ChancesInit.java
+++ src/ChancesInit.java
@@ -1,3 +1,4 @@
+
 /**
  * [ChanesInit.java]
  * Creates an array of methods that can be called
@@ -6,38 +7,80 @@
 
 public class ChancesInit{
 
-    private static Game game;
-    private static Player[] players;
+  private static Game game;
+  private static Player[] players;
 
-    public interface ChanceAction {
-        String execute();
+  public interface ChanceAction {
+    String execute();
+  }
+
+  public static ChanceAction[] chances = new ChanceAction[] {
+    new ChanceAction() { public String execute() { return rainMoney(); } },
+    new ChanceAction() { public String execute() { return misha(); } },
+    new ChanceAction() { public String execute() { return rob(); } },
+      new ChanceAction() { public String execute() { return jailed(); } },
+
+  };
+
+  public static ChanceAction[] wildChances = new ChanceAction[] {
+    new ChanceAction() { public String execute() { return takeL(); } },
+     new ChanceAction() { public String execute() { return backwards(); } },
+  };
+
+  public ChancesInit(Game game){
+    this.game = game;
+    this.players = game.getPlayers();
+    return;
+  }
+
+  public static String rainMoney() { // Regular chance
+    for (int i = 0; i < players.length; i++){
+      players[i].earnCurrency("MIL", 500);
     }
-    public static ChanceAction[] wildChances;
-    public static ChanceAction[] chances;
-
-    public ChancesInit(Game game){
-        this.game = game;
-        this.players = game.getPlayers();
-        chances = new ChanceAction[] {
-            new ChanceAction() { public String execute() { return a(); } },
-        };
-
-        wildChances = new ChanceAction[] {
-            new ChanceAction() { public String execute() { return b(); } },
-        };
-        return;
-    }
-
-    public static String a() {
-        for (int i = 0; i < players.length; i++){
-            players[i].earnCurrency("MIL", 500);
-        }
-        return "Wow! It's raining money, everyone just earned 500 MIL";
-    }
+    return "Wow! It's raining money, everyone just earned 500 MIL";
+  }
 
 
-    public static String b() {
-        // do something dumb
-        return "HOHOHOHHHHOHHOHHOHAHAHAAHAHTROLOLOLLOL";
-    }
-}
+  public static String takeL() { // wild chance]
+    Player currPlayer = players[game.getCurrPlayer()];
+    currPlayer.lose();
+    return "HOHOHOHHHHOHHOHHOHAHAHAAHAHTROLOLOLLOL U DIE \n in other words, you have lost the game. goodbyy";
+  }
+
+
+
+  public static String misha() { // Regular chance
+    Player currPlayer = players[game.getCurrPlayer()];
+    double amt = game.Bitcoin.convertToMIL(currPlayer.getBTC() / 4);
+    currPlayer.convertCurrency("MIL", "BTC", amt);
+    currPlayer.spendCurrency("MIL", amt);
+      return "Misha comes to comp sci class. lose 1/4 of your BTC ";
+      }
+
+ public static String rob() { // Regular chance
+   Player currPlayer = players[game.getCurrPlayer()];
+
+   for (int i = 0; i < players.length; i++){
+     if (i != game.getCurrPlayer()) {
+       players[i].spendCurrency("MIL", 500);
+     }
+   }
+   currPlayer.earnCurrency("MIL",(game.getNumPlayers())*100);
+     return "Steal 100 MIL from everyone";
+ }
+
+
+    public static String jailed() { // chance
+    Player currPlayer = players[game.getCurrPlayer()];
+    currPlayer.goToJail();
+    return "ur evil, get jailed";
+  }
+
+
+    public static String backwards() { // wild chance
+    Player currPlayer = players[game.getCurrPlayer()];
+    currPlayer.goBack();
+    return "you are going backwards now";
+  }
+
+ }
